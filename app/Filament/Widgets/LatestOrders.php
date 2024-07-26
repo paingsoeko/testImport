9d1e5f3fa7f0f9b1594c1526026a1da33624ce49
Paing Soe Ko <social.kopaing@gmail.com>
2024-07-26 12:41:33 +0700

    fix dashboard

--- app/Filament/Widgets/LatestOrders.php
+++ app/Filament/Widgets/LatestOrders.php
@@ -11,44 +11,44 @@ use Squire\Models\Currency;
 
 class LatestOrders extends BaseWidget
 {
-    protected int | string | array $columnSpan = 'full';
+    // protected int | string | array $columnSpan = 'full';
 
-    protected static ?int $sort = 2;
+    // protected static ?int $sort = 2;
 
-    public function table(Table $table): Table
-    {
-        return $table
-            ->query(OrderResource::getEloquentQuery())
-            ->defaultPaginationPageOption(5)
-            ->defaultSort('created_at', 'desc')
-            ->columns([
-                Tables\Columns\TextColumn::make('created_at')
-                    ->label('Order Date')
-                    ->date()
-                    ->sortable(),
-                Tables\Columns\TextColumn::make('number')
-                    ->searchable()
-                    ->sortable(),
-                Tables\Columns\TextColumn::make('customer.name')
-                    ->searchable()
-                    ->sortable(),
-                Tables\Columns\TextColumn::make('status')
-                    ->badge(),
-                Tables\Columns\TextColumn::make('currency')
-                    ->getStateUsing(fn ($record): ?string => Currency::find($record->currency)?->name ?? null)
-                    ->searchable()
-                    ->sortable(),
-                Tables\Columns\TextColumn::make('total_price')
-                    ->searchable()
-                    ->sortable(),
-                Tables\Columns\TextColumn::make('shipping_price')
-                    ->label('Shipping cost')
-                    ->searchable()
-                    ->sortable(),
-            ])
-            ->actions([
-                Tables\Actions\Action::make('open')
-                    ->url(fn (Order $record): string => OrderResource::getUrl('edit', ['record' => $record])),
-            ]);
-    }
+    // public function table(Table $table): Table
+    // {
+    //     return $table
+    //         ->query(OrderResource::getEloquentQuery())
+    //         ->defaultPaginationPageOption(5)
+    //         ->defaultSort('created_at', 'desc')
+    //         ->columns([
+    //             Tables\Columns\TextColumn::make('created_at')
+    //                 ->label('Order Date')
+    //                 ->date()
+    //                 ->sortable(),
+    //             Tables\Columns\TextColumn::make('number')
+    //                 ->searchable()
+    //                 ->sortable(),
+    //             Tables\Columns\TextColumn::make('customer.name')
+    //                 ->searchable()
+    //                 ->sortable(),
+    //             Tables\Columns\TextColumn::make('status')
+    //                 ->badge(),
+    //             Tables\Columns\TextColumn::make('currency')
+    //                 ->getStateUsing(fn ($record): ?string => Currency::find($record->currency)?->name ?? null)
+    //                 ->searchable()
+    //                 ->sortable(),
+    //             Tables\Columns\TextColumn::make('total_price')
+    //                 ->searchable()
+    //                 ->sortable(),
+    //             Tables\Columns\TextColumn::make('shipping_price')
+    //                 ->label('Shipping cost')
+    //                 ->searchable()
+    //                 ->sortable(),
+    //         ])
+    //         ->actions([
+    //             Tables\Actions\Action::make('open')
+    //                 ->url(fn (Order $record): string => OrderResource::getUrl('edit', ['record' => $record])),
+    //         ]);
+    // }
 }
